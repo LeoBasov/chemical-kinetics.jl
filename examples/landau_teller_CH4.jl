1f8f887694bd61bf71b4d561039b7ec7c48d9048
Leo Basov <l.basov@sparc-industries.com>
2024-12-10 20:09:50 +0100

    updated landau-teller test case

--- examples/landau_teller_CH4.jl
+++ examples/landau_teller_CH4.jl
@@ -1,13 +1,13 @@
 using ChemicalKinetics
 using Plots
 
-add_species!("data/N2.json", mole_frac = 1.0)
+add_species!("data/CH4.json", mole_frac = 1.0)
 
 set_T!(10000)
 set_nrho!(1e22)
-set_Tvib!("N2", 5000)
+set_Tvib!("CH4", 5000)
 
-sol = execute(2e-2)
+sol = execute(2e-5)
 
-plot(sol)
-plot!()
+plot()
+plot!(sol);
