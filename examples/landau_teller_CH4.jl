4024518694fed2286f42421cfe6479467a5082da
Leo Basov <basov.leo@gmail.com>
2024-12-12 22:23:41 +0100

    updated solver and test case

--- examples/landau_teller_CH4.jl
+++ examples/landau_teller_CH4.jl
@@ -5,22 +5,11 @@ add_species!("data/CH4.json", mole_frac = 1.0)
 
 set_T!(10000)
 set_nrho!(1e22)
-set_Tvib!("CH4", 5000)
+set_Tvib!("CH4", [5000, 6000, 7000, 8000])
 
-solve!(1e-5)
-
-#sol = get_energy(10)
-
-
+solve!(3e-5)
 
 t, T = get_T(300)
-
-"""sol = ChemicalKinetics._solution
-
-plot(sol.t, sol[1, :])
-plot!(sol.t, sol[2, :])
-plot!(sol.t, sol[3, :])
-plot!(sol.t, sol[4, :])"""
 
 display(plot(t, T))
 
