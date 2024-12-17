7a27a0789367621aba09200f46032707ce1395ed
Leo Basov <leo.basov@dlr.de>
2024-12-17 15:28:14 +0100

    fixed bug in temperature post processing

--- src/ChemicalKinetics.jl
+++ src/ChemicalKinetics.jl
@@ -57,9 +57,10 @@ function get_T(N, species_name)
         push!(T[1], _solution(tt)[1])
 
         for i in 1:Nvibmode
+            mole_fraciont = _state.mole_fractions[species_name]
             theta = _state.species[species_name].vibmodes[i].theta
             degen = _state.species[species_name].vibmodes[i].degen
-            f(x, p = (1, 1)) = calc_evib_kb(x, p) - _solution(tt)[i + _state.offset[species_name]]
+            f(x, p = (1, 1)) = mole_fraciont * calc_evib_kb(x, p) - _solution(tt)[i + _state.offset[species_name]]
             Z = ZeroProblem(f, 1000)
             Tvib = solve(Z, Order1(), p=(theta, degen))
 
