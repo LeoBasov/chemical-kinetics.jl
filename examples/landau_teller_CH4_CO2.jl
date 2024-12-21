ed862424c88fc63c81b5c120ad7086794e6a161d
Leo Basov <leo.basov@dlr.de>
2024-12-21 11:13:04 +0100

    removed old test case and renamed one

--- examples/landau_teller_CH4_CO2.jl
+++ examples/landau_teller_CH4_CO2.jl
@@ -12,12 +12,12 @@ set_nrho!(1e22)
 set_Tvib!("CH4", [5000, 6000, 7000, 8000])
 set_Tvib!("CO2", [2000, 3000, 4000])
 
+execute!(3e-5)
+
 e0 = ChemicalKinetics.calc_etot(ChemicalKinetics._state)
 f(T, p=ChemicalKinetics._state) = ChemicalKinetics.calc_etot(T, p) / e0 - 1.0
 
 Teq = find_zero(f, 5000)
-
-execute!(3e-5)
 
 t, T1 = get_T(300, "CO2")
 t, T2 = get_T(300, "CH4")
