755c2fa0ca375e7c8d4a8f32b6fb6b72650738fe
Leo Basov <leo.basov@dlr.de>
2024-12-21 13:25:59 +0100

    updated test case

--- examples/chemistry_exchange/exchange_reactions.jl
+++ examples/chemistry_exchange/exchange_reactions.jl
@@ -36,10 +36,10 @@ add_species!("data/N.json", mole_frac = 0.2)
 add_species!("data/O2.json", mole_frac = 0.2)
 add_species!("data/O.json", mole_frac = 0.2)
 
+read_reaction!("data/exchange.json")
+
 set_T!(10000)
 set_nrho!(1e23)
-
-read_reaction!("data/exchange.json")
 
 execute!(4e-5)
 
