e4b0df45df20859f0f9166999ab1b63eee53310f
Leo Basov <leo.basov@dlr.de>
2025-01-09 17:06:31 +0100

    updated test cases

--- examples/landau_teller_CH4.jl
+++ examples/landau_teller_CH4.jl
@@ -7,9 +7,10 @@ add_species!("data/CH4.json", mole_frac = 1.0)
 
 set_T!(10000)
 set_nrho!(1e22)
-set_Tvib!("CH4", [5000, 6000, 7000, 8000])
+set_Tvib!("CH4", 5000)
+set_Zvib!("CH4", [20, 30, 40, 50])
 
-execute!(3e-5)
+execute!(1e-5)
 
 t, T = get_T(300)
 t, Tvib = get_Tvib(300, "CH4")
--- examples/landau_teller_CH4_CO2.jl
+++ examples/landau_teller_CH4_CO2.jl
@@ -9,10 +9,12 @@ add_species!("data/CO2.json", mole_frac = 0.5)
 
 set_T!(10000)
 set_nrho!(1e22)
-set_Tvib!("CH4", [5000, 6000, 7000, 8000])
-set_Tvib!("CO2", [2000, 3000, 4000])
+set_Tvib!("CH4", 5000)
+set_Tvib!("CO2", 4000)
+set_Zvib!("CH4", [20, 30, 40, 50])
+set_Zvib!("CO2", [20, 30, 40])
 
-execute!(3e-5)
+execute!(1e-5)
 
 e0 = ChemicalKinetics.calc_etot(ChemicalKinetics._state)
 f(T, p=ChemicalKinetics._state) = ChemicalKinetics.calc_etot(T, p) / e0 - 1.0
@@ -25,10 +27,13 @@ t, T2 = get_Tvib(300, "CH4")
 
 Teq = ones(length(t)) * Teq
 
-plot(t, T)
+p = plot(t, T)
 plot!(t, T1)
 plot!(t, T2)
+plot!(t, Teq, line = (3, :dashdot))
 
-display(plot!(t, Teq, line = (3, :dashdot)))
+display(p)
+
+write2csv("CH4_CO2")
 
 println("done")
